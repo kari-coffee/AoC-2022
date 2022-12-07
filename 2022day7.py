3738ae9543efa8222357caac812718d20ceee439
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-12-07 15:16:35 +0000

    Update 2022day7.py

--- 2022day7.py
+++ 2022day7.py
@@ -1,55 +1,33 @@
-from functools import lru_cache
-
 with open('input.txt') as f:
-    data = [i.strip() for i in f.readlines()]
+    data = [line.strip() for line in f.readlines()]
 
-    dirs_in = {'/':[]}
-    dirs_up = {}
     sizes = {}
-    current = ''
-    for line in data:
-        if '$ cd' in line:
-            if line[5] == '.':
-                if current != '/':
-                    current = dirs_up[current]
-            elif line[5] == '/':
-                current = '/'
-            else:
-                current = line[5:]
-        elif line[:3] == 'dir':
-            dirs_up[line[4:]] = current
-            if current in dirs_in:
-                dirs_in[current].append(line[4:])
-            else:
-                dirs_in[current] = [line[4:]]
-        elif line == '$ ls':
-            continue
-        else:
-            size = line.split()[0]
-            if current in dirs_in:
-                dirs_in[current].append(size)
-            else:
-                dirs_in[current] = [size]
-    ans = 0
-    @lru_cache
-    def find_size(dir):
-        size = 0
-        for i in dirs_in[dir]:
-            if i.isdigit():
-                size += int(i)
-            elif i in sizes:
-                size += sizes[i]
-            else:
-                size += find_size(i)
-        sizes[dir] = size
-        return size
+    path = []
 
-    sizes['/'] = find_size('/')
-    for i in sizes:
-        size = int(sizes[i])
-        if size <= 100000:
-            ans += size
-    total = sum(sizes.values())
-    if total <= 100000:
-        ans += total
+    for line in data:
+        if line[0] == '$':
+            if line == '$ ls':
+                continue
+            elif line == '$ cd ..':
+                path.pop()
+            else:
+                path.append(line[5:])
+                sizes['/'.join(path)] = 0
+        elif line.split()[0].isdigit():
+            size, name = line.split()
+            temp = [i for i in path]
+            while temp:
+                sizes['/'.join(temp)] += int(size)
+                temp.pop()
+    print(sizes)
+    ans = sum([size for size in sizes.values() if size <= 100000])
     print(ans)
+
+    #part 2
+    free = 70000000 - sizes['/']
+    needed = 30000000-free
+    sizes = sorted(list(sizes.values()))
+    for i in sizes:
+        if i > needed:
+            print(i)
+            break
