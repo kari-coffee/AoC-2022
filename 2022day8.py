be1b5547cbf107cf0a662557b51ff3bac66fc6d3
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-12-08 20:26:39 +0000

    Update 2022day8.py

--- 2022day8.py
+++ 2022day8.py
@@ -1,13 +1,73 @@
 with open('input.txt') as f:
-    data = [line.strip() for line in f.readlines()]
-    H = len(data)
-    W = len(data[0])
+    data = [[int(i) for i in line.strip()] for line in f.readlines()]
+    H = len(data)-1
+    W = len(data[0])-1
     ans = 0
-    dp = [] #[left, top] #dps are 2d array of highest tree to the ___ and ___
-    dp2 = [] #[right, bottom]
-    for row in range(1, H-1):
-        for col in range(1, W-1):
+
+    dp = [[[] for j in range(len(data))] for i in range(len(data))] #[left, top] #dps are 2d array of highest tree to the ___ and ___
+    dp2 = [[[] for j in range(len(data))] for i in range(len(data))] #[right, bottom]
+    dp[0][0] = [data[0][0], data[0][0]]
+    dp2[H][W] = [data[-1][-1], data[-1][-1]]
+    for row in range(H+1):
+        for col in range(W+1):
             if row == 0:
-                dp[row, col] = 
-            dp[row, col] = [max(data[row-1], dp[row-1]), max(data[col-1], dp[col-1])]
-            dp2[H-row, W-col] = [max(data[(H-row)+1], dp[(H-row)+1]), max(data[(W-col)+1], dp[(W-col)+1])]
+                if col == 0:
+                    continue
+                dp[row][col] = [10, data[row][col]]
+                dp2[H-row][W-col] = [10, data[H-row][W-col]]
+            elif col == 0:
+                dp[row][col] = [data[row][col], 10]
+                dp2[H-row][W] = [data[H-row][W], 10]
+            else:
+                dp[row][col] = [max(dp[row][col-1][0], data[row][col-1]), max(dp[row-1][col][1], data[row-1][col])]
+                dp2[H-row][W-col] = [max(dp2[(H-row)][(W-col)+1][0], data[H-row][(W-col)+1]), max(dp2[(H-row)+1][(W-col)][1], data[(H-row)+1][W-col])]
+    ans = 0
+    for row in range(1, H):
+        for col in range(1, W):
+            valid = False
+            for i in range(2):
+                if dp[row][col][i] < data[row][col]:
+                    ans += 1
+                    valid = True
+                    break
+            if not valid:
+                for i in range(2):
+                    if dp2[row][col][i] < data[row][col]:
+                        ans += 1
+                        break
+    H += 1
+    W += 1
+    print(ans+W*2+(H-2)*2)
+
+    #part 2
+    ans = 0
+    for row in range(1, H):
+        for col in range(1, W):
+            c = 1
+            # up
+            j = 0
+            for i in range(row-1, -1, -1):
+                if data[i][col] >= data[row][col] or i == 0:
+                    c *= (j+1)
+                    break
+                j += 1
+            j = 0
+            for i in range(row+1, H+1):
+                if data[i][col] >= data[row][col] or i == H:
+                    c *= (j+1)
+                    break
+                j += 1
+            j = 0
+            for i in range(col-1, -1, -1):
+                if data[row][i] >= data[row][col] or i == 0:
+                    c *= (j+1)
+                    break
+                j += 1
+            j = 0
+            for i in range(col+1, W+1):
+                if data[row][i] >= data[row][col] or i == W:
+                    c *= (j+1)
+                    break
+                j += 1
+            ans = max(ans, c)
+    print(ans)
