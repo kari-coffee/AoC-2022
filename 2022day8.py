1c5d02c5c71224c453f2e2f867f843af6786e37b
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-12-08 20:41:58 +0000

    added comments

--- 2022day8.py
+++ 2022day8.py
@@ -11,6 +11,8 @@ with open('input.txt') as f:
     
     for row in range(H+1):
         for col in range(W+1):
+            # if it is an edge: dp[row][col] = data[row][col]
+            
             if row == 0:
                 if col == 0:
                     continue
@@ -19,9 +21,13 @@ with open('input.txt') as f:
             elif col == 0:
                 dp[row][col] = [data[row][col], 10]
                 dp2[H-row][W] = [data[H-row][W], 10]
+            # if it is in middle:
             else:
-                dp[row][col] = [max(dp[row][col-1][0], data[row][col-1]), max(dp[row-1][col][1], data[row-1][col])]
+                dp[row][col] = [max(dp[row][col-1][0], data[row][col-1]), max(dp[row-1][col][1], data[row-1][col])] 
+                #dp of [row][col] = [max(dp[left] and data[left]), max(dp[up]data[up])]
                 dp2[H-row][W-col] = [max(dp2[(H-row)][(W-col)+1][0], data[H-row][(W-col)+1]), max(dp2[(H-row)+1][(W-col)][1], data[(H-row)+1][W-col])]
+                # dp2 is built up the opposite way (from bottom right corner)
+                #dp2 of [row][col] = [max(dp2[right], data[right]), max(dp2[down], data[down])
     ans = 0
     for row in range(1, H):
         for col in range(1, W):
@@ -40,12 +46,11 @@ with open('input.txt') as f:
     W += 1
     print(ans+W*2+(H-2)*2)
 
-    #part 2
+    # lazyman's part 2 - probably could have used a function at least - might try dp ver later
     ans = 0
     for row in range(1, H):
         for col in range(1, W):
             c = 1
-            # up
             j = 0
             for i in range(row-1, -1, -1):
                 if data[i][col] >= data[row][col] or i == 0:
