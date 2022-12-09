d7733a818acaffccad5560ca103dfa0965a94bf3
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-12-09 08:04:09 +0000

    Create 2022day9.py

---
+++ 2022day9.py
@@ -0,0 +1,88 @@
+# with open('input.txt') as f:
+#     data = [line.strip() for line in f.readlines()]
+
+#     visited = set()
+#     head = [0, 0]
+#     tail = [0, 0]
+
+#     def chase():
+#         x = head[0]-tail[0]
+#         y = head[1]-tail[1]
+#         if set([abs(x), abs(y)]) == set([1,2]):
+#             if abs(x) > 1:
+#                 x //= 2
+#             else:
+#                 y //= 2
+#             tail[0] += x
+#             tail[1] += y
+#         elif abs(x) == 2:
+#             tail[0] += (x//2)
+#         elif abs(y) == 2:
+#             tail[1] += (y//2)
+#         if tuple(tail) not in visited:
+#             visited.add(tuple(tail))
+
+#     for line in data:
+#         d, v = line.split()
+#         v = int(v)
+#         if d == 'R':
+#             for i in range(v):
+#                 head[0] += 1
+#                 chase()
+#         elif d == 'L':
+#             for i in range(v):
+#                 head[0] -= 1
+#                 chase()
+#         elif d == 'U':
+#             for i in range(v):
+#                 head[1] += 1
+#                 chase()
+#         else:
+#             for i in range(v):
+#                 head[1] -= 1
+#                 chase()
+# print(len(visited))
+
+with open('input.txt') as f:
+    data = [line.strip() for line in f.readlines()]
+
+    visited = set()
+    rope = [[0,0] for _ in range(10)]
+
+    def chase(head, tail):
+        x = head[0]-tail[0]
+        y = head[1]-tail[1]
+        if set([abs(x), abs(y)]) == set([1,2]):
+            if abs(x) > 1:
+                x //= 2
+            else:
+                y //= 2
+            tail[0] += x
+            tail[1] += y
+        elif abs(x) == 2:
+            tail[0] += (x//2)
+        elif abs(y) == 2:
+            tail[1] += (y//2)
+        if tuple(tail) not in visited:
+            visited.add(tuple(tail))
+        return(head, tail)
+    ix = 0
+    while ix < len(data):
+        
+            d, v = data[ix].split()
+            v = int(v)
+            cur = rope[knot]
+            for i in range(v):
+                for knot in range(10):
+                    if d == 'R':
+                            cur[0] += 1
+                    elif d == 'L':
+                            cur[0] -= 1
+                    elif d == 'U':
+                            cur[1] += 1
+                            
+                    else:
+                            cur[1] -= 1
+                    chase(rope[knot+1], cur)
+            ix += 1
+print(len(visited))
